51e86f779bd8973575b731db94430e2ea8252141
joyounin <3723519@naver.com>
2023-02-22 14:52:58 +0900

    javascript 객체#2

--- javascript-practice/basics/ex02.js
+++ javascript-practice/basics/ex02.js
@@ -23,6 +23,23 @@
 - function
 */
 
+/*
+    자바스크립트 객체를 분류하는 또 다른 방법
+
+    1. 내장 객체
+        - JavaScript 엔진 내부에 미리 내장(정의)되어 있는 객체
+        - Number, Boolean, Date, RegDate, Array, .... : 생성자 함수
+        - parseInt, setTimeout,  setInerval           : 일반함수
+    
+    2. 호스트 객체
+        - JavaScript 엔진이 embed 되어 있는 호스트 환경에 있는 객체
+        - Brower가 호스트인 경우 : screen, location, navigator, document(DOM), XmlHttpRequest
+        - Node가 호스트인 경우: http, os, fs, path, process, ...
+
+    3. 사용자 객체
+        - 자바스크립트 엔진이 실행되면서 생성하는 객체
+*/
+
 console.log("=== 기본타입(undefined, number, string, boolean) ===");
 var u = undefined;  // var u; 와 동일, 선언과 정의를 구분하지 않는다.
 var i = 10;
