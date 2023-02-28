23a8083554d6f7f3ffcca57d73ce13dc3d4f4b3d
joyounin <3723519@naver.com>
2023-02-28 11:27:55 +0900

    ajax #4

--- ch08/src/main/java/ch08/controller/api/ApiController.java
+++ ch08/src/main/java/ch08/controller/api/ApiController.java
@@ -1,17 +1,40 @@
 package ch08.controller.api;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import ch08.dto.JsonResult;
+import ch08.dto.XmlResult;
 import ch08.vo.GuestbookVo;
 
 @Controller
 @RequestMapping("/api")
 public class ApiController {
+	
+	@ResponseBody
+	@RequestMapping(value="/text", method=RequestMethod.GET)
+	public String text() {
+		return "Text 데이터";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/html", method=RequestMethod.GET)
+	public String html() {
+		return "<h1>Ajax연습</h1><p>HTML 데이터</p>";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/xml", method=RequestMethod.GET)
+	public XmlResult xml() {
+		XmlResult.GuestbookVo vo = new XmlResult.GuestbookVo();
+		vo.setNo(1L);
+		vo.setName("둘리");
+		vo.setMessage("호이~");
+		
+		return XmlResult.success(vo);
+	}
 	
 	@ResponseBody
 	@RequestMapping(value="/json", method=RequestMethod.GET)
